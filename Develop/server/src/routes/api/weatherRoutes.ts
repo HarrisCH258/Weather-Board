fac8cc545fb9caf1fc06b0f31859fd803764051b
CH258 <ch2953har@gmail.com>
2024-10-25 03:49:27 -0400

    fix syntax errors

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,25 +1,28 @@
 import { Router, type Request, type Response } from 'express';
+
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post()
-router.post('/', (req: Request, res: Response) => {
+//router.post()
+router.post('/', (req: Request, _res: Response) => {
   // TODO: GET weather data from city name
   try {
     const {cityName} = req.body;
-    
+    console.log(cityName);
+  }catch(err){
+    console.log(err)
   }
 
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, _res: Response) => {});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (_req: Request, _res: Response) => {});
 
 export default router;
--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+
 dotenv.config();
+
 
 // Import the routes
 import routes from './routes/index.js';
@@ -10,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
+app.use(express.static('../../client/dist'));
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 
